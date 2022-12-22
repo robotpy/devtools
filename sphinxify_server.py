bdae26757a926a19759d6dc1ef5a7f0a789d8de8
Dustin Spicuzza <dustin@virtualroadside.com>
2022-12-22 12:02:57 -0500

    Output raw also, make boxes bigger

--- sphinxify_server.py
+++ sphinxify_server.py
@@ -14,9 +14,11 @@ index_html = b"""
     <head></head>
     <body>
     In: doxygen contents<br/>
-    <textarea id="inbox" rows="10" cols="100"></textarea><br/>
+    <textarea id="inbox" rows="20" cols="100"></textarea><br/>
     Out: python docstring<br/>
-    <textarea id="outbox" rows="10" cols="100"></textarea>
+    <textarea id="outbox" rows="20" cols="100"></textarea><br/>
+    Out: raw<br/>
+    <textarea id="rawbox" rows="20" cols="100"></textarea>
 
     <script>
 
@@ -29,6 +31,7 @@ index_html = b"""
         }).then(res => {
             res.json().then(res => {
                 outbox.value = res.outbox;
+                rawbox.value = res.rawbox;
             })
         });
     }
@@ -59,8 +62,9 @@ class SphinxifyAPI(BaseHTTPRequestHandler):
         if self.path == "/sphinxify":
             length = int(self.headers["content-length"])
             postdata = json.loads(self.rfile.read(length).decode("utf-8"))
-            result = sphinxify.process(postdata["inbox"])
-            response = json.dumps(dict(outbox=result))
+            docstring = sphinxify.process(postdata["inbox"])
+            raw = sphinxify.process_raw(postdata["inbox"])
+            response = json.dumps(dict(outbox=docstring, rawbox=raw))
 
             self.send_response(200)
             self.send_header("Content-Type", "application/json")
